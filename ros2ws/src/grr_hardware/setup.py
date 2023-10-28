643fa145d6a0e39cad1688becee0d7fa02e8a827
Philip Smith <philip@randomsmiths.com>
2023-10-28 00:54:44 -0400

    include hardware map in build

--- ros2ws/src/grr_hardware/setup.py
+++ ros2ws/src/grr_hardware/setup.py
@@ -10,7 +10,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['package.xml', 'hardware_map.yaml']),
         ('share/' + package_name + '/launch/', ['launch/teleop.launch.py']),
     ],
     install_requires=['setuptools'],
