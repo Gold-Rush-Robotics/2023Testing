8c51032c373ea1ec377ac2b9360963899f1294a4
Philip Smith <philip@randomsmiths.com>
2023-10-28 00:54:59 -0400

    fix bugs in drive that prevented launch

--- ros2ws/src/grr_hardware/grr_hardware/drive.py
+++ ros2ws/src/grr_hardware/grr_hardware/drive.py
@@ -1,19 +1,21 @@
 from typing import List
 import rclpy
 import yaml
-from rclpy import Node
+from rclpy.node import Node
 from rclpy.context import Context
 from rclpy.parameter import Parameter
 from geometry_msgs.msg import Twist
 from grr_interfaces.msg import Motor, MotorSet, Enable
 
+import os
+
 class Drive(Node):
-    def __init__(self, node_name: str, *, context: Context = None, cli_args: List[str] = None, namespace: str = None, use_global_arguments: bool = True, enable_rosout: bool = True, start_parameter_services: bool = True, parameter_overrides: List[Parameter] = None, allow_undeclared_parameters: bool = False, automatically_declare_parameters_from_overrides: bool = False) -> None:
-        super().__init__(node_name, context=context, cli_args=cli_args, namespace=namespace, use_global_arguments=use_global_arguments, enable_rosout=enable_rosout, start_parameter_services=start_parameter_services, parameter_overrides=parameter_overrides, allow_undeclared_parameters=allow_undeclared_parameters, automatically_declare_parameters_from_overrides=automatically_declare_parameters_from_overrides)
+    def __init__(self) -> None:
+        super().__init__("drive")
         self.subcription = self.create_subscription(Twist, "cmd_vel", self.drive_callback, 10)
         self.motor_publish = self.create_publisher(MotorSet, "grr_motors", 10)
         self.subcription
-        with open("../resource/hardware_map.yaml", "r") as f:
+        with open("/home/grr/2023Testing/ros2ws/install/grr_hardware/share/grr_hardware/hardware_map.yaml", "r") as f:
             self.HWMAP = yaml.safe_load(f.read())
     
     def drive_motors(self, fl:float, fr:float, bl:float, br:float):
