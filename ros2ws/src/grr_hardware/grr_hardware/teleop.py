8eefca173717c714886b1a77336e98bf52996b4d
Philip Smith <philip@randomsmiths.com>
2023-10-28 01:59:50 -0400

    enable drivetrain if moved

--- ros2ws/src/grr_hardware/grr_hardware/teleop.py
+++ ros2ws/src/grr_hardware/grr_hardware/teleop.py
@@ -7,12 +7,14 @@ from rclpy.parameter import Parameter
 
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist, Vector3
+from grr_interfaces.msg import Enable
 
 class Teleop(Node):
     def __init__(self) -> None:
         super().__init__("GRR_Teleop")
         self.subscriber = self.create_subscription(Joy, "joy", self.joyCallback, 10)
         self.twistPub = self.create_publisher(Twist, "cmd_vel", 10)
+        self.enablePub = self.create_publisher(Enable, "grr_enable", 10)
         
     def joyCallback(self, data:Joy):
         lside = data.axes[0]
@@ -22,8 +24,13 @@ class Teleop(Node):
         strafe = lside
         forward = lup
         rotate = rside
+        if(strafe or forward or rotate):
+            self.enablePub.publish(Enable(attachment=5, enable=True))
+
         twist = Twist(linear=Vector3(x=strafe, y=forward,z=0.0), angular=Vector3(x=0.0, y=0.0, z=rotate))
         self.twistPub.publish(twist)
+        
+            
         #self.get_logger().info(f"\nforward = {forward : .2f}\nstrafe = {strafe : .2f}\nrotate = {rotate : .2f}")
 
 def main():
