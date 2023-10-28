67ce777ae7b87e2b226556715ea16bf61a213c9a
Philip Smith <philip@randomsmiths.com>
2023-10-28 00:02:30 -0400

    create a launch file (matches source)

--- ros2ws/src/grr_hardware/setup.py
+++ ros2ws/src/grr_hardware/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os, glob
 
 package_name = 'grr_hardware'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch/', ['launch/teleop.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
